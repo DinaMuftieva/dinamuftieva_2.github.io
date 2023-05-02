4ba058075c6f2bfd6ddb792678db79b340e83a7a
DinaMuftieva <100910082+DinaMuftieva@users.noreply.github.com>
2023-05-02 10:48:37 +0300

    Add files via upload

---
+++ sankey_main.js
@@ -0,0 +1,112 @@
+var getScriptPromisify = (src) => {
+  return new Promise(resolve => {
+    $.getScript(src, resolve)
+  })
+}
+
+(function () {
+  const prepared = document.createElement('template')
+  prepared.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `
+  class SamplePrepared extends HTMLElement {
+    constructor () {
+      super()
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+
+      this._root = this._shadowRoot.getElementById('root')
+
+      this._props = {}
+
+      this.render()
+    }
+
+    onCustomWidgetResize (width, height) {
+      this.render()
+    }
+
+    async render () {
+      await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+
+      const chart = echarts.init(this._root)
+      myChart.showLoading();
+$.get(ROOT_PATH + 'https://echarts.apache.org/examples/data/asset/data/product.json', function (data) {
+  myChart.hideLoading();
+  myChart.setOption(
+    (const option = {
+      title: {
+        text: 'Sankey Diagram'
+      },
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove'
+      },
+      series: [
+        {
+          type: 'sankey',
+          data: data.nodes,
+          links: data.links,
+          emphasis: {
+            focus: 'adjacency'
+          },
+          levels: [
+            {
+              depth: 0,
+              itemStyle: {
+                color: '#fbb4ae'
+              },
+              lineStyle: {
+                color: 'source',
+                opacity: 0.6
+              }
+            },
+            {
+              depth: 1,
+              itemStyle: {
+                color: '#b3cde3'
+              },
+              lineStyle: {
+                color: 'source',
+                opacity: 0.6
+              }
+            },
+            {
+              depth: 2,
+              itemStyle: {
+                color: '#ccebc5'
+              },
+              lineStyle: {
+                color: 'source',
+                opacity: 0.6
+              }
+            },
+            {
+              depth: 3,
+              itemStyle: {
+                color: '#decbe4'
+              },
+              lineStyle: {
+                color: 'source',
+                opacity: 0.6
+              }
+            }
+          ],
+          lineStyle: {
+            curveness: 0.5
+          }
+        }
+      ]
+    })
+  );
+});
+      chart.setOption(option)
+    }
+  }
+
+  customElements.define('com-sap-sample-echarts-sankey', SamplePrepared)
+})()
